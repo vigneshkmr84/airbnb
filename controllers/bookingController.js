44f33ae7d0185da7bc2a3ae4803236d49c953d66
Vignesh <vigneshkmr84@gmail.com>
2022-09-18 03:34:07 -0500

    Booking Controller modified for user_id to fetch from request (jwt token)

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,33 +3,40 @@ const bson = require('bson');
 var bookingModel = require('../model/bookings');
 var propertyModel = require('../model/property');
 
+const jsonResponse = (response, status) => {
+    return { status: status, message: response };
+}
+
+const Internal_Server_Error = jsonResponse("Internal Server Error.", 500);
+
+
 // based on the query parameter ?past=true/false it fetches the history and future bookings
 // if nothing is passed all bookings are fetched
 const getBookings = async (req, res) => {
 
-    try{
+    try {
         let past = req.query.past;
         let query = {};
         console.log(past);
         let user_id = req.params.user_id;
         console.log("Get bookings for user : " + user_id);
-        if ( typeof past === 'undefined' ){
+        if (typeof past === 'undefined') {
             console.log("Fetching All bookings");
-            query = { user_id : new bson.ObjectId(user_id) };
-        }else if ( past === 'true'){
+            query = { user_id: new bson.ObjectId(user_id) };
+        } else if (past === 'true') {
             console.log("Fetching past bookings");
-            query = { user_id : new bson.ObjectId(user_id), start_date : {$lte : new Date().toISOString()  } };
-        }else if ( past === 'false'){
+            query = { user_id: new bson.ObjectId(user_id), start_date: { $lte: new Date().toISOString() } };
+        } else if (past === 'false') {
             console.log("Fetching Future bookings");
-            query = { user_id : new bson.ObjectId(user_id), start_date : {$gte : new Date().toISOString() } };
+            query = { user_id: new bson.ObjectId(user_id), start_date: { $gte: new Date().toISOString() } };
         }
         console.log(query);
-        let result = await bookingModel.find( query );
+        let result = await bookingModel.find(query);
         console.log("Total fetched bookings : " + result.length);
-        res.status(200).send(result);
-    }catch(e){
-        console.log("Error occurred during fetch : " + e );
-        res.status(500).send("Internal Server Error.");
+        res.status(200).send(jsonResponse(result, 200));
+    } catch (e) {
+        console.log("Error occurred during fetch : " + e);
+        res.status(500).send(Internal_Server_Error);
     }
 };
 
@@ -37,45 +44,50 @@ const getBookings = async (req, res) => {
 const createBooking = async (req, res) => {
     console.log("Reserve a booking");
     let body = req.body;
-
-    try{
+    console.log(body)
+    try {
         // need to check for existing reservation 
         // for the property within the given time frame
-        
+
         // Logic
         // will append the start time and end time of the property listed 
         // with the start date and end date of the booking 
         // will be easy to calculate when on exit
-        let property = await propertyModel.findOne({_id : new bson.ObjectId(body.property_id)});
+        let property = await propertyModel.findOne({ _id: new bson.ObjectId(body.property_id) });
+        /* body.start_date = getDateWithTime(property.checkin_time, body.start_date);
+        body.end_date = getDateWithTime(property.checkout_time, body.end_date); */
+        // console.log(property)
         body.start_date = getDateWithTime(property.checkin_time, body.start_date);
         body.end_date = getDateWithTime(property.checkout_time, body.end_date);
         calculated_cost = calculateTotalCost(property, body.start_date, body.end_date);
         body.total_cost = calculated_cost[0];
         body.taxes = calculated_cost[1];
+        body.user_id = req.user_id;
+        body.no_of_people = Number(body.no_of_people)
 
         var newReservation = new bookingModel(body);
 
-        await newReservation.save(function(err,doc) {
+        await newReservation.save(function (err, doc) {
             if (!err) {
                 id = doc._id;
                 console.log("Booking ID : " + id);
-                res.status(200).send("Booked Successfully : " + id);
-            }else{
+                res.status(200).send(jsonResponse("Booked Successfully : " + id, 200));
+            } else {
                 console.error("Error occurred in reservation : " + err);
-                res.status(500).send("Error occurred in reservation.");
+                res.status(500).send(jsonResponse("Unable to Reserve", 500));
             }
         });
 
-    }catch(e){
+    } catch (e) {
         console.log("Error in Reservation " + e);
-        res.status(500).send("Internal Server Error.");
+        res.status(500).send(Internal_Server_Error);
     }
 };
 
-const getDateWithTime = (totalMinutes, newDate) =>{
+const getDateWithTime = (totalMinutes, newDate) => {
 
     let hours = 0, minutes = 0;
-    hours = Math.floor( totalMinutes/60 );
+    hours = Math.floor(totalMinutes / 60);
     minutes = totalMinutes % 60;
     newDate = new Date(newDate);
     newDate.setHours(hours, minutes, 0);
@@ -84,13 +96,20 @@ const getDateWithTime = (totalMinutes, newDate) =>{
 
 //const calculateTotalCost = (start_date, end_date, property_id) => {
 const calculateTotalCost = (property, start_date, end_date) => {
-    days_count = Math.ceil((end_date.getTime() - start_date.getTime())/(1000*3600*24));
+    /* console.log(start_date)
+    console.log(end_date) */
+    start_date = new Date(start_date)
+    end_date = new Date(end_date)
+    /* console.log(start_date)
+    console.log(end_date) */
+    days_count = Math.ceil((end_date.getTime() - start_date.getTime()) / (1000 * 3600 * 24));
+    console.log(days_count)
     total_cost = property.cost_per_day * days_count + property.service_cost + property.cleaning_cost;
     // tax calculation logic = min of 150$ or 11% of the total cost 
-    taxes = Math.min(150, total_cost*0.11);
-    total_cost = total_cost+taxes;
+    taxes = Math.min(150, total_cost * 0.05);
+    total_cost = total_cost + taxes;
     console.log("Total Cost for " + days_count + " days : " + total_cost);
-    return [ total_cost, taxes];
+    return [total_cost, taxes];
 }
 
 // cancel existing booking
@@ -101,25 +120,25 @@ const cancelBooking = async (req, res) => {
     console.log("Cancel Existing booking : " + bookingId);
     let filter = { _id: new bson.ObjectId(bookingId) };
     let update = { canceled: true };
-    try{
-        let booked = await bookingModel.findOne({ _id: new bson.ObjectId(bookingId)});
+    try {
+        let booked = await bookingModel.findOne({ _id: new bson.ObjectId(bookingId) });
         //console.log(booked);
-        if ( isValidCancelation(booked.start_date) ){
+        if (isValidCancelation(booked.start_date)) {
             await bookingModel.findOneAndUpdate(
                 filter,
                 update,
-                {new : true}
+                { new: true }
             );
             console.log("Canceled Successfully");
-            res.status(200).send("Canceled Successfully");
-        }else{
+            res.status(200).send(jsonResponse("Canceled Successfully", 200));
+        } else {
             console.log("Cancelation time is less than 48 hours.");
-            res.status(400).send("Cancelation time is less than 48 hours.");
+            res.status(400).send(jsonResponse("Cancelation time is less than 48 hours.", 400));
         }
-        
-    }catch(e){
+
+    } catch (e) {
         console.log("Error in cancelling booking " + e);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send(Internal_Server_Error);
     }
 };
 
@@ -136,8 +155,8 @@ const isValidCancelation = (start_date) => {
 
     if (start_date > current_date)
         return true;
-    
+
     return false;
 }
 
-module.exports={getBookings, createBooking, cancelBooking};
+module.exports = { getBookings, createBooking, cancelBooking };
--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -1,16 +1,26 @@
 const jwt = require('jsonwebtoken');
 
-const expiry = "10h";
+const expiry = '30h';
+const access_token_secret = 'D7F38637-4F25-4309-853B-EFDEC17B8BA1'
 
-// const generateToken = (userDetails) => {
+const jsonResponse = (response, status) => {
+    return JSON.stringify({ status: status, message: response });
+}
+
+const Internal_Server_Error = jsonResponse("Internal Server Error.", 500);
+
+
+/* Function that generates the JWT token
+Embeds user_id and is_host details into
+the token, which can be used later */
 function generateToken(userDetails) {
 
     console.log("Generating JWT for user : " + userDetails._id);
 
     try {
         return jwt.sign(
-            { userId: userDetails._id, is_host: userDetails.is_host },
-            "Pas$worD",
+            { user_id: userDetails._id, is_host: userDetails.is_host },
+            access_token_secret,
             { expiresIn: expiry }
         );
     } catch (e) {
@@ -19,4 +29,37 @@ function generateToken(userDetails) {
     }
 }
 
-module.exports = { generateToken };
+/* Middleware layer that can be added 
+to all Routes which will authenticate
+based on the validity of the user
+and check if valid, will let the user 
+into the controller, also adds the 
+user_id and is_host parameter into the 
+request, which can be captured and used */
+function authenticateToken(req, res, next) {
+    console.log("Validating Token")
+    try {
+        const header = req.headers['authorization'];
+        const token = header && header.split(' ')[1]
+
+        if (token === null || token === undefined) {
+            console.log("Missing Token")
+            return res.status(401).send(jsonResponse("Missing Token", 401));
+        }
+
+        jwt.verify(token, access_token_secret, (err, { user_id, is_host }) => {
+            if (err) {
+                console.error("Error occurred during validation : " + err);
+                res.status(401).send(jsonResponse("Invalid Token", 401))
+            }
+            console.log("Valid Token")
+            req.user_id = user_id;
+            req.is_host = is_host;
+            next();
+        })
+    } catch (ex) {
+        console.error("Exception occurred during validation " + ex);
+        res.status(500).send(Internal_Server_Error)
+    }
+}
+module.exports = { generateToken, authenticateToken };
